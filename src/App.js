5b2a2b08b88f112913463cc48715fa5027a0a8b6
Ibristi <nursi27n48@gmail.com>
2022-01-13 21:34:12 +0300

    USUAL, before breaking into components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import './App.css'
 
-
 function App() {
-	return <div className='App'>
-  </div>
+	return (
+		<div className='app-wrapper'>
+			<header className='header'>
+				<img src='https://logopond.com/logos/939617960dc5a56f126ff574f0616f31.png' />
+			</header>
+			<nav className='nav'>
+				<div>
+					<a>VENI</a>
+				</div>
+				<div>
+					<a>VIDI</a>
+				</div>
+				<div>
+					<a>VICI</a>
+				</div>
+				<div>
+					<a>LOVE</a>
+				</div>
+				<div>
+					<a>LIFE</a>
+				</div>
+			</nav>
+			<div className='content'>
+				<div>
+					<img src='https://www.ukinbound.org/wp-content/uploads/2020/10/header-image-33-875x350.png' />
+				</div>
+        <div>
+          Ava + desc
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+			</div>
+		</div>
+	)
 }
 
 export default App
