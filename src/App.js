c228816d938ae39baa6f937ecfb089525993995f
Ibristi <nursi27n48@gmail.com>
2022-01-21 17:35:09 +0300

    sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css'
 
-
 function App() {
-	return <div className='App'>
-		HELLO WORLD
-  </div>
+	return (
+		<div className='App'>
+			<form className='form'>
+				<div className='container'>
+					<h1>SIGN UP</h1>
+					<div className='name-part'>
+						Full name:
+						<input type='text' placeholder='Ф.И.О' />
+					</div>
+					<div className='username-part'>
+						Nickname:
+						<input type='text' placeholder='Nickname' />
+					</div>
+					<div className='email-part'>
+						Email:
+						<input type='email' placeholder='Email' />
+					</div>
+					<button className='btn'>Confirm</button>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default App
